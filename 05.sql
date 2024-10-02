db28c76f4e77e7cba75b91867f20e685f72edc0d
Kushal Bansal <kushal.bansal.1001@gmail.com>
2024-10-02 13:29:04 +0545

    folder structure rarranged

--- 05.sql
+++
@@ -1,2 +0,0 @@
- -- leetcode 1683. Invalid Tweets
- select tweet_id from tweets where char_length(content) > 15    
---
+++ Select/05.sql
@@ -0,0 +1,4 @@
+ -- leetcode 1683. Invalid Tweets
+--  select tweet_id from tweets where char_length(content) > 15  
+                        -- or  
+ select tweet_id from tweets where length(content) > 15    
