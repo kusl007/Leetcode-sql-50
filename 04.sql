aa9fbd373c2dd904b7bfb63158a3d726b8a83f5f
Kushal Bansal <kushal.bansal.1001@gmail.com>
2024-10-02 13:23:08 +0545

    problem 2 3 4 5  completed

---
+++ 04.sql
@@ -0,0 +1,2 @@
+--leetcode 1148. Article Views I
+select distinct author_id as id from views where author_id = viewer_id order by author_id
---
+++ 05.sql
@@ -0,0 +1,2 @@
+ -- leetcode 1683. Invalid Tweets
+ select tweet_id from tweets where char_length(content) > 15    
